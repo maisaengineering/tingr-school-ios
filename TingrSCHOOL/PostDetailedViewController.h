769235bd7f9c8d61eb4672185ae7dbb53046f446
maisapride <maisapride7@maisaprides-MacBook-Pro.local>
2018-02-02 10:04:54 +0530

    Updates for Deep linking

---
+++ TingrSCHOOL/PostDetailedViewController.h
@@ -0,0 +1,13 @@
+//
+//  PostDetailedViewController.h
+//  TingrSCHOOL
+//
+//  Created by maisapride on 01/02/18.
+//  Copyright © 2018 Maisa Pride. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PostDetailedViewController : UIViewController
+
+@end
---
+++ TingrSCHOOL/PostDetailedViewController.m
@@ -0,0 +1,37 @@
+//
+//  PostDetailedViewController.m
+//  TingrSCHOOL
+//
+//  Created by maisapride on 01/02/18.
+//  Copyright © 2018 Maisa Pride. All rights reserved.
+//
+
+#import "PostDetailedViewController.h"
+
+@interface PostDetailedViewController ()
+
+@end
+
+@implementation PostDetailedViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
